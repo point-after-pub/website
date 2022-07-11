633e4c543588770ac11e8a03020ca560d21098bd
Matthew Gile <e10051@connexuscu.org>
2022-07-11 15:25:18 -0500

    slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,36 @@
+const images_path = '../images'
+
+class Slideshow{
+    constructor() {
+        let images_path = 'images/'
+        this.images = [
+          images_path + 'glasses.png',
+          images_path + 'soup.png',
+        ]
+
+        this.current_index = 0
+      }
+
+    change(){
+      if ( this.current_index >= this.images.length - 1){
+        this.current_index = 0
+      }
+      else{
+        this.current_index += 1
+      }
+
+      $('#pa-slideshow').attr('src', this.images[this.current_index])
+    }
+
+    start(){
+        setInterval(this.change.bind(this), 5000)
+    }
+
+    stop(){
+
+    }
+}
+
+slideshow = new Slideshow()
+
+slideshow.start()
